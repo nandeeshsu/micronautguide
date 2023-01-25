1133583e8a4d3e22eace7165623eace5b3295a53
Nandeesh Subbegowda <subbegowda@gmail.com>
2023-01-25 21:27:35 +0000

    flyway changes and lombok config change

---
+++ micronautguide1/src/main/resources/db/migration/V6__schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS book;
+
+CREATE TABLE IF NOT EXISTS public.book
+(
+    id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
+    name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT book_pkey PRIMARY KEY (id),
+    CONSTRAINT name_unique UNIQUE (name)
+)
+
+ALTER TABLE IF EXISTS public.book OWNER to postgres;
